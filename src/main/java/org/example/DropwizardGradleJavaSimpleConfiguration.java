bc5e1d2645f240f9783ef009d43f4f92f91c53f6
Vinay Kumar Pabba <vinaykumarpabba66@gmail.com>
2025-06-28 22:42:07 +0530

    Add sample configuration parameters

--- src/main/java/org/example/DropwizardGradleJavaSimpleConfiguration.java
+++ src/main/java/org/example/DropwizardGradleJavaSimpleConfiguration.java
@@ -1,8 +1,34 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.core.Configuration;
+import jakarta.validation.constraints.NotEmpty;
 
 public class DropwizardGradleJavaSimpleConfiguration extends Configuration {
 
+    @NotEmpty
+    private String question;
 
+    @NotEmpty
+    private String greetingTemplate;
+
+    @JsonProperty
+    public String getQuestion() {
+        return question;
+    }
+
+    @JsonProperty
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    @JsonProperty
+    public String getGreetingTemplate() {
+        return greetingTemplate;
+    }
+
+    @JsonProperty
+    public void setGreetingTemplate(String greetingTemplate) {
+        this.greetingTemplate = greetingTemplate;
+    }
 }
